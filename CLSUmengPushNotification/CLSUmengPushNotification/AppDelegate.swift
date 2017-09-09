d07d351e16e06ac3e0b4614cb6cd662174c756cc
StephenChen <tasb00429@gmail.com>
2017-09-09 23:45:18 +0800

    [ Add ]：Codes, Images

--- CLSUmengPushNotification/CLSUmengPushNotification/AppDelegate.swift
+++ CLSUmengPushNotification/CLSUmengPushNotification/AppDelegate.swift
@@ -14,32 +14,136 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.setupPushNotification(launchOptions: launchOptions)
+    
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
+// ******************************************
+//
+// MARK: - 1.推播 iOS 8 ~ 10 基本設定
+//
+// ******************************************
+extension AppDelegate {
+    
+    fileprivate func setupPushNotification(launchOptions: [UIApplicationLaunchOptionsKey: Any]?) {
+        
+        UMessage.start(withAppkey: "Your Key", launchOptions: launchOptions)
+        UMessage.registerForRemoteNotifications()
+        
+        /// iOS 10 以上，包含 iOS 10
+        if #available(iOS 10, *) {
+            
+            let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+            let center = UNUserNotificationCenter.current()
+            center.delegate = self
+            UNUserNotificationCenter.current().requestAuthorization(options: options, completionHandler: { (granted, error) in
+                
+                if let error = error {
+                    print("iOS 申請推播權限錯誤 \(error.localizedDescription)")
+                    return
+                }
+                
+                if granted {
+                    print("點擊允許")
+                } else {
+                    print("點擊不允許")
+                }
+            })
+        } else {
+            
+        }
+        
+        /// 打开日志，方便调试
+        UMessage.setLogEnabled(true)
+        /// 关闭 U-Push 自带的弹出框
+        UMessage.setAutoAlert(false)
+    }
+    
+    /// 拿到 Device Token
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("1.2.7版本开始不需要用户再手动注册devicetoken，SDK会自动注册 deviceToken = \(deviceToken.hexString)")
+    }
+    
+    /// 獲取錯誤日誌
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("推播錯誤 \(error.localizedDescription)")
+    }
+}
+
+// ******************************************
+//
+// MARK: - 2.推播 iOS 8 ~ 9 實作方法
+//
+// ******************************************
+extension AppDelegate {
+
+    /// iOS 8, 9 以下使用这个方法接收通知
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+        /// 统计点击数
+        UMessage.didReceiveRemoteNotification(userInfo)
+    }
+}
+
+// ******************************************
+//
+// MARK: - 3.推播 iOS 10 以上實作方法
+//
+// ******************************************
+@available(iOS 10.0, *)
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    /// iOS10 新增：當 App 在＊＊前景＊＊模式下會收到訊息
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        /// 推播的消息
+        let userInfo: [AnyHashable: Any] = notification.request.content.userInfo
+        print("推播的消息 = \(userInfo)")
+        
+        if notification.request.trigger?.isKind(of: UNPushNotificationTrigger.self) ?? false {
+           print("當 App 在＊＊前景＊＊模式下，處理遠程推送")
+        } else {
+           print("當 App 在＊＊前景＊＊模式下，處理本地推送")
+        }
+        
+        /// 统计点击数
+        UMessage.didReceiveRemoteNotification(userInfo)
+        
+        completionHandler([.alert, .sound, .badge])
+    }
+    
+    /// iOS10 新增：當 App 在＊＊背景＊＊模式下
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        /// 推播的消息
+        let userInfo: [AnyHashable: Any] = response.notification.request.content.userInfo
+        print("推播的消息 = \(userInfo)")
+
+        /// 统计点击数
+        UMessage.didReceiveRemoteNotification(userInfo)
+        
+        if response.notification.request.trigger?.isKind(of: UNPushNotificationTrigger.self) ?? false {
+            print("當 App 在＊＊背景＊＊模式下，處理遠程推送")
+        } else {
+            print("當 App 在＊＊背景＊＊模式下，處理本地推送")
+        }
+    }    
+}
+
+// ******************************************
+//
+// MARK: - Helper (寫出來是因為之後可以給別的地方用)
+//
+// ******************************************
+extension Data {
+    
+    /// Convert Data into String
+    public var hexString: String {
+        return withUnsafeBytes {(bytes: UnsafePointer<UInt8>) -> String in
+            let buffer = UnsafeBufferPointer(start: bytes, count: count)
+            return buffer.map {String(format: "%02hhx", $0)}.reduce("", { $0 + $1 })
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Umeng-PushNotification-Swift
 實作友盟推播 ( iOS 8 以上 )
+
+
+# 一：註冊友盟帳號
+
+註冊友盟帳號
+
+# 二：安裝 SDK 到專案
+
+首先下載 [v1.5.0a版本(支援到 iOS 10)](http://dev.umeng.com/push/ios/integration)
+
+![](https://github.com/5SMNOONMS5/CLS-Umeng-PushNotification-Swift/images/1.png)
+
+![](https://github.com/5SMNOONMS5/CLS-Umeng-PushNotification-Swift/images/2.png)
+
+![](https://github.com/5SMNOONMS5/CLS-Umeng-PushNotification-Swift/images/3.png)
+
+
+# 三：複製貼上 （記得要改Appkey）
+
+
+我將 Code 分為三大部分，如果當前專案只支援到 iOS 9，那就複製 **第1** 跟 **第2**，如果是從 iOS 8 開始支援，那就全部都複製。
+
+![](https://github.com/5SMNOONMS5/CLS-Umeng-PushNotification-Swift/images/4.png)
+
+# 四：把 DeviceToken 貼到官網上，然後完畢。
+
+![](https://github.com/5SMNOONMS5/CLS-Umeng-PushNotification-Swift/images/5.png)
+
+
+# 五：進階版本
+
